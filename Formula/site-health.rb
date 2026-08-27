a4530871008bc231666f0a236bbff4248fcd7507
Atilla Gündüz <gunduz.atilla@icloud.com>
2026-08-27 13:10:08 +0300

    site-health 0.9.4

--- Formula/site-health.rb
+++ Formula/site-health.rb
@@ -1,8 +1,8 @@
 class SiteHealth < Formula
   desc "Fast CLI for checking website and domain health"
   homepage "https://github.com/atillalab/site-health"
-  url "https://github.com/atillalab/site-health/archive/refs/tags/v0.9.3.tar.gz"
-  sha256 "0caa8a297698fd8893d633ff632a4112958923fa6279bb3274c9f47afaee21d7"
+  url "https://github.com/atillalab/site-health/archive/refs/tags/v0.9.4.tar.gz"
+  sha256 "2b6130583c1daa834d8035151d67bc6d121adbcb48749fb4533de3c0d8210361"
   license "MIT"
 
   depends_on "go" => :build
